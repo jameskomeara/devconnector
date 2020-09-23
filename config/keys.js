c0cc2d4a44449998263f627dc477cad037e47df2
jameskomeara <jamesomeara2000@gmail.com>
2020-09-23 19:24:42 +0100

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://james:adminpassword@mern-dev-connecter.sym9f.mongodb.net/mern-dev-connecter?retryWrites=true&w=majority',
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const users = require('./routes/api/users')
+const posts = require('./routes/api/posts')
+const profile = require('./routes/api/profile')
+
+const app = express()
+
+// DB Config
+const db = require('./config/keys').mongoURI
+
+// Connect to MongoDB
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello, James')
+})
+
+// Use Routes
+app.use('/api/users', users)
+app.use('/api/posts', posts)
+app.use('/api/profile', profile)
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`server running on port ${port}`))
